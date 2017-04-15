e99e0b8d10630485863cc5755367f5ea0876a881
David Cha <Cha4998@Davids-MacBook-Pro.local>
2017-04-15 16:53:57 -0500

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Styles from './index.iosStyles';
+
+export default class reactNativePractice extends Component {
+	render() {
+		return (
+			<View style={Styles.container}>
+				<Text style={Styles.welcome}>
+					Welcome!
+				</Text>
+				<Text style={Styles.instructions}>
+					To get started, edit index.ios.js
+				</Text>
+				<Text style={Styles.instructions}>
+					Press Cmd+R to reload,{'\n'}
+					Cmd+D or shake for dev menu
+				</Text>
+			</View>
+		);
+	}
+}
+
+AppRegistry.registerComponent('reactNativePractice', () => reactNativePractice);
---
+++ index.iosStyles.js
@@ -0,0 +1,21 @@
+const Styles = {
+	container: {
+		backgroundColor: 'black',
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	welcome: {
+		color: 'white',
+		fontSize: 40,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#fff',
+		marginBottom: 5,
+	}
+}
+
+export default Styles;
